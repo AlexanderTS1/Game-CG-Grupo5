9f4d8b6c9dd445769bca7555e180f1b81a8baf29
Alex-Helder <72510437+AlexHelder-Tyzer@users.noreply.github.com>
2020-10-19 15:25:51 -0500

    Plane with mov and colors

---
+++ Plane/utils1.py
@@ -0,0 +1,154 @@
+# pip install PyOpenGL
+# pip install pygame
+# pip install pygame==2.0.0.dev6 (for python 3.8.x)
+# pip install numpy
+# Python 3.8
+
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+import pygame
+from pygame.locals import *
+
+import math
+import random as rdn
+import numpy as np
+
+### Algorithm ###
+
+def set_pixel(x, y, r, g, b, size):
+	glColor3f(r, g, b)
+	glPointSize(size)
+
+	glBegin(GL_POINTS)
+	glVertex2f(x, y)
+	glEnd()
+	
+	# print("{}\t{}".format(x, y))
+	# pygame.time.wait(100)
+
+	# option 1 (ok)
+	#pygame.display.flip()
+	
+	# option 2
+	glFlush()
+
+def color_pixel(width, height, x, y, size):
+	rgb = glReadPixels(width / 2 + x , height / 2 + y, size ,size , 
+						GL_RGB, GL_UNSIGNED_BYTE, None)
+	return list(rgb)
+
+def clearCanvas():
+	glClearColor(0.0, 0.0, 0.0, 1.0)
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+def Traslate(vertices, tx, ty):
+	T = [
+		[1, 0, tx], 
+		[0, 1, ty], 
+		[0, 0, 1]
+	]
+	result = []
+	for item in vertices:
+		point = np.dot(T, item)
+		result.append(point)
+	return result
+
+def Scale(vertices, Sx,Sy):
+	R = [
+		[Sx, 0, 0], 
+		[0, Sy, 0],
+		[0, 0, 1]
+	]
+	result = []
+	for item in vertices:
+		point = np.dot(R, item)
+		result.append(point)
+	return result
+
+"""def Defender(x, y, r, g, b, size):
+	matrix = [
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+		[0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+		[0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+		[1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0],
+		[1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0],
+		[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0]
+	]
+
+	for i in range(len(matrix)):
+		for j in range(len(matrix[0])):
+			if matrix[i][j] == 1:
+				set_pixel(y - j, x - i, r, g, b, size) """
+
+def MovePlane(x, y, sx, sy,size):
+	clearCanvas()
+	vertices = Traslate([[x, y, 1]], sx, sy)
+	x = vertices[0][0]
+	y = vertices[0][1]
+	Plane(y, -x, size)
+	pygame.display.flip()
+	return x, y
+
+### Draw
+def display_openGL(width, height, scale):
+	pygame.display.set_mode((width, height), pygame.OPENGL)
+
+	glClearColor(0.0, 0.0, 0.0, 1.0)
+	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+	# glScalef(scale, scale, 0)
+
+	gluOrtho2D(-1 * width / 2, width / 2, -1 * height / 2, height / 2)
+
+def Plane(x, y, size):
+	matrix = [
+		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0],
+		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0],
+		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0],
+		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0],
+		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0],
+		[0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,3,0,0],
+		[0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,1,1,3,3,1,0],
+		[0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,3,1,3,1,1],
+		[0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+		[0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+		[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+		[0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+		[0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0]
+	]
+
+	for i in range(len(matrix)):
+		for j in range(len(matrix[0])):
+
+			if matrix[i][j] == 1:
+				r,g,b = 0,0,1
+				set_pixel(y - j, x - i, r, g, b, size)
+			if matrix[i][j] == 2:
+				r, g, b = 1,1,0
+				set_pixel(y - j, x - i, r, g, b, size)
+			if matrix[i][j] == 3:
+				r, g, b = 1,0,0
+				set_pixel(y - j, x - i, r, g, b, size)
